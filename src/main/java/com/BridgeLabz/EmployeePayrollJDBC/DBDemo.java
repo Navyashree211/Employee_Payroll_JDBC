6b6d5488cef04e8d8f3e10d1a866af684268013c
unknown <navyas211@gmail.com>
2021-11-07 23:27:31 +0530

    [NAVAYA]ADDED: To retrieve the EmployeePayroll from the database

--- src/main/java/com/BridgeLabz/EmployeePayrollJDBC/DBDemo.java
+++ src/main/java/com/BridgeLabz/EmployeePayrollJDBC/DBDemo.java
@@ -1,5 +1,5 @@
 /*
- * UC1:- TO Create a payroll service database and have java program connect to database. 
+ * UC2:- Ability for Employee_Payroll_Service to retrieve the EmployeePayroll from the database. 
  * 
  * @author : Navaya Shree
  * @since : 7-11-2021
@@ -7,44 +7,91 @@
 package com.BridgeLabz.EmployeePayrollJDBC;
 
 import java.sql.DriverManager;
-import java.util.Enumeration;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 import java.sql.Connection;
-import java.sql.Driver;
 
-public class DBDemo {
-
-	public static void main(String[] args) {
+public class DBDemo {   
+	public static void main(String[] args) throws SQLException {
 
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 		String userName = "root";
 		String password = "1234";
-		Connection con;
-		try {
+		Statement stmt = null;
+		Connection con = null;
+		ResultSet rs = null;
 
+		String qry = "select * from employee_payroll.payroll_service";
+		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Driver loaded");
+			System.out.println("Driver Class Loaded");
 
-		} catch (ClassNotFoundException e) {
-			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
-		}
-		listDrivers();
-
-		try {
-
-			System.out.println("Connecting to database" + jdbcURL);
 			con = DriverManager.getConnection(jdbcURL, userName, password);
-			System.out.println("Connection is successfull!!!!" + con);
+			System.out.println("Connetion Establish with db server");
 
-		} catch (Exception e) {
+			stmt = con.createStatement();
+
+			rs = stmt.executeQuery(qry);
+
+			System.err.println("id-> " + "EmployeesName-> " + "mobileNumber-> " + "address-> " + "       department-> "
+					+ "gender-> " + " basic_pay-> " + "deductions-> " + "taxable_pay-> " + "tax-> " + "met_pay-> "
+					+ "start");
+
+			while (rs.next()) {
+				int id = rs.getInt("Id");
+				String EmployeesName = rs.getString(2);
+				long mobileNumber = rs.getLong(3);
+				String address = rs.getString(4);
+				String department = rs.getString(5);
+				String gender = rs.getString(6);
+				double basic_pay = rs.getDouble(7);
+				double deductions = rs.getDouble(8);
+				double taxable_pay = rs.getDouble(9);
+				double tax = rs.getDouble(10);
+				double met_pay = rs.getDouble(11);
+				Date start = rs.getDate(12);
+
+				System.out.println();
+
+				System.out.println(id + "->" + EmployeesName + "->" + mobileNumber + "->" + address + "->" + department
+						+ "->" + gender + "->" + basic_pay + "->" + deductions + "->" + taxable_pay + "->" + tax + "->"
+						+ met_pay + "->" + start);
+			}
+
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
+
+		finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+
 	}
 
-	private static void listDrivers() {
-		Enumeration<Driver> driverList = DriverManager.getDrivers();
-		while (driverList.hasMoreElements()) {
-			Driver driverClass = (Driver) driverList.nextElement();
-			System.out.println("listDriver " + driverClass.getClass().getName());
-		}
-	}
 }
