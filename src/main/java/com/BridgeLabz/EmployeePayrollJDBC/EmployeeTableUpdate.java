439523ad656797d430fc7311467f68c684ffe751
unknown <navyas211@gmail.com>
2021-11-12 21:11:10 +0530

    [NAVYA]ADDED: Ability to update the salary i.e. the base pay for Employee Terisa to 3000000 Database using JDBC PreparedStatement.

--- src/main/java/com/BridgeLabz/EmployeePayrollJDBC/EmployeeTableUpdate.java
+++ src/main/java/com/BridgeLabz/EmployeePayrollJDBC/EmployeeTableUpdate.java
@@ -1,45 +1,56 @@
 /*
- * UC3:- Ability to update the salary i.e. the base pay for Employee Terisa to 3000000.00 and sync it with Database . 
+ * UC4:- Ability to update the salary i.e. the base pay for 
+ *       Employee Terisa to 3000000 Database using JDBC PreparedStatement. 
  * 
  * @author : Navaya Shree
- * @since : 7-11-2021
+ * @since : 12-11-2021
  */
 package com.BridgeLabz.EmployeePayrollJDBC;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class EmployeeTableUpdate {  
-	public static void main(String[] args) throws SQLException {
+public class EmployeeTableUpdate {
+	static Connection con;
 
-		Connection con = null;
-		Statement stmt = null;
+	public static void main(String[] args) throws Exception , SQLException {   
+		
+	
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
 		String qry = "update employee_payroll.payroll_service set basic_pay=3000000.00 where name='Terisa'";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
 			System.out.println("Driver Class Loaded");
 
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=1234");
+			con = ConnectionDB.createCP();
 
 			System.out.println("Connetion Establish with db server");
 
-			stmt = con.createStatement();
-			System.out.println("Platform Created");
-
-			stmt.executeUpdate(qry);
+			pstmt = con.prepareStatement(qry);
 			System.out.println("Data Update");
-
+			
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 
 		finally {
-			if (stmt != null) {
+			if (rs != null) {
 				try {
-					stmt.close();
-					System.out.println("Closed All Resources");
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -48,13 +59,14 @@ public class EmployeeTableUpdate {
 			if (con != null) {
 				try {
 					con.close();
-					System.out.println("Closed All Resources");
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 
 			}
+			System.out.println("Closed All Resources");
 		}
 
 	}
 }
+
